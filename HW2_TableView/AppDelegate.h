66c8371fcaa48fcba0573e54c619bbacc48076f0
Edgar González <EdgarGonzalez@MacBook-Pro-de-Edgar.local>
2014-10-05 20:32:40 -0500

    Initial Commit

---
+++ HW2_TableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HW2_TableView
+//
+//  Created by Edgar González on 05/10/14.
+//  Copyright (c) 2014 Edgar González. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HW2_TableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HW2_TableView
+//
+//  Created by Edgar González on 05/10/14.
+//  Copyright (c) 2014 Edgar González. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
